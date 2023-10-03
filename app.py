0413f3354e14bb6ffb656876fa35f16efd036ef0
Ulisses Junior <ulissesjunior2005@gmail.com>
2023-10-03 09:25:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config("ECOP06")
+
+st.title("Aula de ECOP06")
+
+st.subheader("Felipão CAlvão Loirão")
+
+
+st.markdown('''
+# Unidos por uma causa
+''')
+
+st.image('https://media.licdn.com/dms/image/C4E03AQGFCM6GTwwpHg/profile-displayphoto-shrink_800_800/0/1654196983646?e=2147483647&v=beta&t=BsqsYoeetMZzx6XfE8udGPJNr0HMSDpUyTU3XjcNj6U')
+
+st.image('https://www.lance.com.br/galerias/wp-content/uploads/2021/10/memes-santos-na-zona-de-rebaixamento-9-667x474.jpg')
+
+st.image('https://github.com/valerio-unifei.png')
+
+if st.button('Pressione aqui'):
+    st.text("Santos Série B 2023")
