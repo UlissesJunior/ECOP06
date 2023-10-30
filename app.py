423b3e19179cb1659410425e82f2339aab29be21
Ulisses Junior <ulissesjunior2005@gmail.com>
2023-10-30 14:04:54 +0000

    feat: aadd gunicorn

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# Rota: HOME ou Principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
     nome = None
     if request.method == 'POST' and 'nome' in request.form:
-        nome = request.form['nome']
+      nome = request.form['nome']
     return render_template('index.html', nome=nome)
 
 if __name__ == "main":
-    app.run(debug=True)
+   app.run(debug=True)
